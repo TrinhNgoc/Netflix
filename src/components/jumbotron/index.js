becb541b4fb0f17a525de197debb9e201ecdfdfa
Unknown <jennyngu@hawaii.edu>
2021-01-25 18:03:50 -1000

    Accordion Component

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -9,24 +9,24 @@ export default function Jumbotron ({ children, direction = 'row', ...restProps }
       </Inner>
     </Item>
   )
-}
+};
 
 Jumbotron.Container = function JumbotronContainer({children, ...restProps}) {
   return <Container {...restProps}>{children}</Container>
-}
+};
 
 Jumbotron.Pane = function JumbotronPane({children, ...restProps}) {
   return <Pane {...restProps}>{children}</Pane>
-}
+};
 
 Jumbotron.Title = function JumbotronTitle({children, ...restProps}) {
   return <Title {...restProps}>{children}</Title>
-}
+};
 
 Jumbotron.SubTitle = function JumbotronSubTitle({children, ...restProps}) {
   return <SubTitle {...restProps}>{children}</SubTitle>
-}
+};
 
 Jumbotron.Image = function JumbotronImage({children, ...restProps}) {
   return <Image {...restProps} />;
-}
+};
